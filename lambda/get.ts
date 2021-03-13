182ce6587b9951e88ff758c103014fd14b88750d
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-13 22:08:53 +0000

    Init

--- lambda/get.ts
+++ lambda/get.ts
@@ -1,9 +1,14 @@
-import { APIGatewayProxyEvent } from 'aws-lambda'
+// import { APIGatewayProxyEvent } from 'aws-lambda'
+import got from 'got'
 
-exports.handler = async function (event: APIGatewayProxyEvent) {
+exports.handler = async function () {
+  const response = await got(
+    'https://api.themoviedb.org/3/movie/76341?api_key=bd7dd01f64dfe4718c76a679170134d0',
+  )
+
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, CDK! You've hit ${event.body}\n`,
+    body: `Hello, CDK! You've hit ${JSON.stringify(response.body)}\n`,
   }
 }
