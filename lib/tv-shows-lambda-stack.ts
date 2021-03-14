f0cedde9998049fd2f20cb6d31b70e3c71b5efa9
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-14 19:24:20 +0000

    Adds aws sdk & lambda execution role

--- lib/tv-shows-lambda-stack.ts
+++ lib/tv-shows-lambda-stack.ts
@@ -1,14 +1,27 @@
-import * as cdk from '@aws-cdk/core'
+import { Stack, App, StackProps } from '@aws-cdk/core'
 import { Function, Runtime, Code } from '@aws-cdk/aws-lambda'
+import { Role, ServicePrincipal, PolicyStatement } from '@aws-cdk/aws-iam'
 
-export class TvShowsLambdaStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+export class TvShowsLambdaStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props)
+
+    const executionRole = new Role(this, 'ExecutionRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    })
+
+    executionRole.addToPolicy(
+      new PolicyStatement({
+        resources: ['*'],
+        actions: ['ses:SendEmail', 'ses:SendRawEmail'],
+      }),
+    )
 
     new Function(this, 'GetMoviesHandler', {
       runtime: Runtime.NODEJS_12_X,
       code: Code.fromAsset('dist'),
       handler: 'get.handler',
+      role: executionRole,
     })
   }
 }
