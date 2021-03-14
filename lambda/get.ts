cc52dc33507650fa376c8b0db8ca780f994aa38a
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-14 12:14:32 +0000

    Refactors

--- lambda/get.ts
+++ lambda/get.ts
@@ -1,11 +1,25 @@
 import got from 'got'
 
+const baseURL = 'https://api.themoviedb.org/3/tv'
+
 const tvShows: { name: string; id: string }[] = [
   {
     name: 'succession',
     id: '76331',
   },
 ]
+
+type GetShowsWithDetails = (apiKey: string) => TVShows.ShowsWithDetails
+
+const getShowsWithDetails: GetShowsWithDetails = apiKey =>
+  Promise.all(
+    tvShows.map(async movie => {
+      const { body } = await got(`${baseURL}/${movie.id}?api_key=${apiKey}`)
+      const movieDetails: TVShows.APIResponse = JSON.parse(body)
+
+      return { name: movie.name, lastAirDate: movieDetails.last_air_date }
+    }),
+  )
 
 export const handler: TVShows.Handler = async function () {
   const apiKey = process.env.DATABASE_API_KEY
@@ -15,16 +29,7 @@ export const handler: TVShows.Handler = async function () {
   }
 
   try {
-    const tvShowsWithDetails: TVShows.ShowsWithDetails = Promise.all(
-      tvShows.map(async movie => {
-        const { body } = await got(`https://api.themoviedb.org/3/tv/${movie.id}?api_key=${apiKey}`)
-        const movieDetails: TVShows.APIResponse = JSON.parse(body)
-
-        return { name: movie.name, lastAirDate: movieDetails.last_air_date }
-      }),
-    )
-
-    console.log('HELLO', await tvShowsWithDetails)
+    console.log('HELLO', await getShowsWithDetails(apiKey))
   } catch (error) {
     throw new Error(error)
   }
