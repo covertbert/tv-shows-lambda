23bf8a59f17facbebcfa57e694a223e7fef3b1e6
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-14 21:19:24 +0000

    Adds send email tests

--- lambda/utils/send-email.test.ts
+++ lambda/utils/send-email.test.ts
@@ -3,7 +3,7 @@ import { SES } from 'aws-sdk'
 import { sendEmail } from '.'
 
 describe('sendEmail', () => {
-  it('calls SES send email', async () => {
+  it('calls SES send email with parameters as email body', async () => {
     SES.prototype.sendEmail = jest.fn().mockReturnValue({
       promise: jest.fn().mockResolvedValue({
         ResponseMetadata: { RequestId: 'abc-123' },
@@ -11,9 +11,18 @@ describe('sendEmail', () => {
       }),
     })
 
-    const ses = new SES()
-    await sendEmail([{ name: 'hello', lastAirDate: '1234' }])
+    const expectedShows = [{ name: 'hello', lastAirDate: '1234' }]
 
-    expect(ses.sendEmail).toHaveBeenCalled()
+    const ses = new SES()
+    await sendEmail(expectedShows)
+
+    expect(ses.sendEmail).toHaveBeenCalledWith({
+      Destination: { ToAddresses: ['success@simulator.amazonses.com'] },
+      Message: {
+        Body: { Text: { Data: JSON.stringify(expectedShows) } },
+        Subject: { Data: 'Test Email' },
+      },
+      Source: 'info@bertie.dev',
+    })
   })
 })
