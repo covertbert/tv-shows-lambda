292ad8c93d447a514acffa1cdfcc5846e1baf463
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-14 14:08:14 +0000

    Adds cdk tests

---
+++ lib/tv-shows-lambda.test.ts
@@ -0,0 +1,11 @@
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as TvShowsLambda from './tv-shows-lambda-stack'
+
+test('TvShowsLambdaStack', () => {
+  const app = new cdk.App()
+  const stack = new TvShowsLambda.TvShowsLambdaStack(app, 'MyTestStack')
+
+  expectCDK(stack).to(haveResource('AWS::IAM::Role'))
+  expectCDK(stack).to(haveResource('AWS::Lambda::Function'))
+})
