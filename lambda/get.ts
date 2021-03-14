a89e4cff152180328cc711ab2a7fb403d1e2b606
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-14 11:51:50 +0000

    MVP lambda

--- lambda/get.ts
+++ lambda/get.ts
@@ -1,14 +1,39 @@
-// import { APIGatewayProxyEvent } from 'aws-lambda'
 import got from 'got'
 
 exports.handler = async function () {
-  const response = await got(
-    'https://api.themoviedb.org/3/movie/76341?api_key=bd7dd01f64dfe4718c76a679170134d0',
-  )
+  const movies: { name: string; id: string }[] = [
+    {
+      name: 'succession',
+      id: '76331',
+    },
+  ]
 
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, CDK! You've hit ${JSON.stringify(response.body)}\n`,
+  const apiKey = process.env.DATABASE_API_KEY
+
+  if (!apiKey) {
+    throw new Error('Movie DB API key missing')
+  }
+
+  try {
+    const moviesWithDetails = Promise.all(
+      movies.map(async movie => {
+        const { body } = await got(`https://api.themoviedb.org/3/tv/${movie.id}?api_key=${apiKey}`)
+        const movieDetails: TVShows.Response = JSON.parse(body)
+
+        return { name: movie.name, lastAirDate: movieDetails.last_air_date }
+      }),
+    )
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/plain' },
+      body: JSON.stringify(await moviesWithDetails),
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'A BIG ERROR',
+    }
   }
 }
