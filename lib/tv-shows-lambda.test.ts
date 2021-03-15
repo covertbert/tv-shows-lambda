76c2731af21fe14e4e48f5df45c0e90c9ed167a1
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-15 11:17:12 +0000

    Expands CDK unit tests

--- lib/tv-shows-lambda.test.ts
+++ lib/tv-shows-lambda.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert'
+import { expect as expectCDK, haveResource, haveResourceLike } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import * as TvShowsLambda from './tv-shows-lambda-stack'
 
@@ -7,5 +7,15 @@ test('TvShowsLambdaStack', () => {
   const stack = new TvShowsLambda.TvShowsLambdaStack(app, 'MyTestStack', { apiKey: '123' })
 
   expectCDK(stack).to(haveResource('AWS::IAM::Role'))
-  expectCDK(stack).to(haveResource('AWS::Lambda::Function'))
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Lambda::Function', {
+      Handler: 'get.handler',
+    }),
+  )
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Events::Rule', {
+      ScheduleExpression: 'cron(* 10 ? * TUE *)',
+      State: 'ENABLED',
+    }),
+  )
 })
