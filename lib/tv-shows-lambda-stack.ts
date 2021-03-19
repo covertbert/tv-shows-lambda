ce851b97ca1bd134f7bcb48c2591b485a25ee817
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-19 22:44:59 +0000

    Certificates and targets

--- lib/tv-shows-lambda-stack.ts
+++ lib/tv-shows-lambda-stack.ts
@@ -5,6 +5,8 @@ import { Rule, Schedule } from '@aws-cdk/aws-events'
 import { LambdaFunction } from '@aws-cdk/aws-events-targets'
 import { LambdaRestApi } from '@aws-cdk/aws-apigateway'
 import { ARecord, RecordTarget, PublicHostedZone } from '@aws-cdk/aws-route53'
+import { ApiGateway } from '@aws-cdk/aws-route53-targets'
+import { Certificate } from '@aws-cdk/aws-certificatemanager'
 
 interface ExtendedStackProps extends StackProps {
   apiKey: string
@@ -66,6 +68,15 @@ export class TvShowsLambdaStack extends Stack {
       proxy: false,
     })
 
+    api.addDomainName('DomainName', {
+      domainName: 'shows.bertie.dev',
+      certificate: Certificate.fromCertificateArn(
+        this,
+        'Certificate',
+        'arn:aws:acm:us-east-1:515213366596:certificate/904b7400-ca9a-4f45-8f77-91deccfd79c1',
+      ),
+    })
+
     const shows = api.root.addResource('shows')
     shows.addMethod('GET')
     shows.addMethod('POST')
@@ -74,7 +85,7 @@ export class TvShowsLambdaStack extends Stack {
 
     new ARecord(this, 'AliasRecord', {
       zone,
-      target: RecordTarget.fromIpAddresses(api.url),
+      target: RecordTarget.fromAlias(new ApiGateway(api)),
       recordName: 'shows.bertie.dev',
       ttl: Duration.seconds(60),
     })
