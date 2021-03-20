d2bf2bb4c87ddee16c80405b1f983b42bc14280e
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-20 22:33:27 +0000

    Refactors get lambda stack variables

--- lib/constructs/get-lambda.ts
+++ lib/constructs/get-lambda.ts
@@ -27,30 +27,30 @@ export class GetLambda extends Construct {
       },
     })
 
-    const api = new LambdaRestApi(this, 'GetMoviesAPI', {
-      handler: getLambda,
-      proxy: false,
-    })
-
     const certificate = new Certificate(this, 'Certificate', {
       domainName: 'bertie.dev',
       subjectAlternativeNames: ['*.bertie.dev'],
       validationMethod: ValidationMethod.DNS,
     })
 
-    api.addDomainName('DomainName', {
+    const getApi = new LambdaRestApi(this, 'GetMoviesAPI', {
+      handler: getLambda,
+      proxy: false,
+    })
+
+    getApi.addDomainName('DomainName', {
       domainName: 'shows.bertie.dev',
       certificate: certificate,
     })
 
-    const shows = api.root.addResource('shows')
+    const shows = getApi.root.addResource('shows')
     shows.addMethod('GET')
 
     const zone = PublicHostedZone.fromLookup(this, 'HostedZone', { domainName: 'bertie.dev' })
 
     new ARecord(this, 'AliasRecord', {
       zone,
-      target: RecordTarget.fromAlias(new ApiGateway(api)),
+      target: RecordTarget.fromAlias(new ApiGateway(getApi)),
       recordName: 'shows.bertie.dev',
       ttl: Duration.seconds(60),
     })
