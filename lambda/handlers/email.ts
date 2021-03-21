4516d84bd2d13afffba3c4663444eacef8aa614f
Bertie Blackman <blackmanrgh@gmail.com>
2021-03-21 14:10:37 +0000

    Better error handling in email handler

--- lambda/handlers/email.ts
+++ lambda/handlers/email.ts
@@ -7,15 +7,15 @@ export const handler: Handler = async () => {
   const apiKey = process.env.DATABASE_API_KEY
   const recipientEmails = process.env.RECIPIENT_EMAILS
 
-  if (!apiKey) {
-    throw new Error('Movie DB API key missing')
-  }
-
-  if (!recipientEmails) {
-    throw new Error('Recipient emails missing')
-  }
-
   try {
+    if (!apiKey) {
+      throw new Error('Movie DB API key missing')
+    }
+
+    if (!recipientEmails) {
+      throw new Error('Recipient emails missing')
+    }
+
     const showsWithTheirDetails = await getShowsWithDetails(TV_SHOWS, BASE_URL, apiKey)
     const withRecentEpisodes = showsWithTheirDetails.filter(show => hasNewEpisode(show.lastAirDate))
 
