3267529bb438a4642a491b16f1c9f397c50dec27
Bertie Blackman <blackmanrgh@gmail.com>
2021-04-28 20:49:50 +0100

    Adds stryker mutation tests

---
+++ stryker.conf.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  files: ['*.js', '*.json', 'lambda/**/*.ts'],
+  mutate: ['lambda/**/*.ts', '!**/*.test.ts'],
+  testRunner: 'jest',
+  coverageAnalysis: 'off',
+  packageManager: 'yarn',
+  reporters: ['html', 'clear-text', 'progress'],
+  htmlReporter: { baseDir: 'build/reports/stryker' },
+  tsconfigFile: 'tsconfig.json',
+  timeoutMS: 60000,
+  thresholds: { high: 100, low: 95, break: 95 },
+  disableTypeChecks: 'lambda/**/*.ts',
+}
