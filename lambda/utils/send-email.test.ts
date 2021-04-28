a59ab039ebdd0eb2410aeb46464d818e69015522
Bertie Blackman <blackmanrgh@gmail.com>
2021-04-28 22:09:55 +0100

    Test SES constructor calls

--- lambda/utils/send-email.test.ts
+++ lambda/utils/send-email.test.ts
@@ -2,22 +2,30 @@ import { SES } from 'aws-sdk'
 
 import { sendEmail } from '.'
 
+jest.mock('aws-sdk')
+
+const sesMock = (SES as unknown) as jest.Mock
+
 describe('sendEmail', () => {
   it('calls SES send email with parameters as email body', async () => {
-    SES.prototype.sendEmail = jest.fn().mockReturnValue({
+    const sendSesEmail = jest.fn().mockReturnValue({
       promise: jest.fn().mockResolvedValue({
         ResponseMetadata: { RequestId: 'abc-123' },
         MessageId: 'cba-321',
       }),
     })
 
+    sesMock.mockImplementationOnce(() => ({
+      sendEmail: sendSesEmail,
+    }))
+
     const expectedMessageBody = 'Hello there'
     const expectedEmailAddresses = ['dog@cat.com']
 
-    const ses = new SES()
     await sendEmail(expectedMessageBody, expectedEmailAddresses)
 
-    expect(ses.sendEmail).toHaveBeenCalledWith({
+    expect(sesMock).toHaveBeenCalledWith({ region: 'us-east-1' })
+    expect(sendSesEmail).toHaveBeenCalledWith({
       Destination: { ToAddresses: expectedEmailAddresses },
       Message: {
         Body: { Text: { Data: expectedMessageBody } },
